001eb170bccf3b76d54cca0240f5cc2e9f4a20d2
ravindrajnv18-oss <ravindra.jnv18@gmail.com>
2026-07-21 11:55:02 +0530

    Add files via upload

---
+++ Snowflake/03_Create_Schemas.sql
@@ -0,0 +1,9 @@
+/*============================================================
+ Step : Create Schemas
+=============================================================*/
+
+CREATE SCHEMA IF NOT EXISTS RAW;
+
+CREATE SCHEMA IF NOT EXISTS STAGING;
+
+CREATE SCHEMA IF NOT EXISTS CURATED;
